1a0b33787ccff36c9ff3ca77d4a8bb7645b90c53
Unknown <pablo.osorio@xmarts.com>
2018-09-06 20:24:49 -0500

    Ckeckbox para saber si es un trabajador de hospital

--- oehealth/oeh_medical/models/oeh_medical.py
+++ oehealth/oeh_medical/models/oeh_medical.py
@@ -855,5 +855,10 @@ class OeHealthVaccines(models.Model):
 class AddFieldUsersOehealth(models.Model):
     _name='res.users'
     _inherit='res.users'
-    hospital_usuario = fields.Many2one('oeh.medical.health.center', string='Hospital donde trabaja', help="Medical Center", required=True)
-    
+    hospital_employee=fields.Boolean(string="¿Trabaja en algun hospital?",default=False)
+    hospital_usuario = fields.Many2one('oeh.medical.health.center', string='Hospital donde trabaja', help="Medical Center")
+
+    @api.onchange('hospital_employee')
+    def onchange_hospital_employee(self):
+        if self.hospital_employee==False:
+            self.hospital_usuario=''
